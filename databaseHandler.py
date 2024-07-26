9de445a7123bba814e66121f7aaa9748ca4af8af
RobertRiggs22 <105801248+RobertRiggs22@users.noreply.github.com>
2024-07-26 15:24:53 -0400

    initial commit

---
+++ databaseHandler.py
@@ -0,0 +1,126 @@
+from pymongo import MongoClient
+import json
+
+class databaseHandler:
+
+    def __init__(self):
+            #Connect to server
+            client = MongoClient("localhost", 27017)
+            self.db = client.get_database("bookstore")
+
+            #gets the next user id to be given out
+            maxUID = self.db.users.find_one(sort=[("uid", -1)])
+            if maxUID == None:
+                self.userID = 1
+            else:
+                self.userID = maxUID["uid"] + 1
+            #gets the next book id to be given out
+            maxBID = self.db.books.find_one(sort=[("bid", -1)])
+            if maxBID == None:
+                self.bookID = 1
+            else:
+                self.bookID = maxBID["bid"] + 1
+            #gets the next order id to be given out
+            maxOID = self.db.orders.find_one(sort=[("oid", -1)])
+            if maxOID == None:
+                self.orderID = 1
+            else:
+                self.orderID = maxOID["oid"] + 1
+
+    def createUser(self, username, password, role):
+        new_user = {"uid": self.userID, "username": username, "password": password, "role": role}
+        self.db.users.insert_one(new_user)
+        self.userID = self.userID + 1
+
+    def createBook(self, title, description, pagecount, price):
+        new_book = {"bid": self.bookID, "title": title, "description": description, "pagecount": pagecount, "price": price}
+        self.db.books.insert_one(new_book)
+        self.bookID = self.bookID + 1
+
+    def createOrder(self, uid, bid):
+        new_order = {"oid": self.orderID, "uid": uid, "bid": bid}
+        self.db.orders.insert_one(new_order)
+        self.orderID = self.orderID + 1
+
+    def allUsers(self):
+        users = self.db.users.find({})
+        for user in users:
+            print(user)
+
+    def allBooks(self):
+        books = self.db.books.find({})
+        for book in books:
+            print(book)
+
+    def allOrders(self):
+        orders = self.db.orders.find({})
+        for order in orders:
+            print(order)
+
+    def updateUser(self, uid, username, password, role):
+        try:
+            user_to_update = {"uid": int(uid)}
+            updated_user = {"$set": {"username": username, "password": password, "role": role}}
+            self.db.users.update_one(user_to_update, updated_user)
+        except:
+            input("Invalid Input. Press enter to continue.")
+
+    def updateBook(self, bid, title, description, pagecount, price):
+        try:
+            book_to_update = {"bid": int(bid)}
+            updated_book = {"$set": {"title": title, "description": description, "pagecount": pagecount, "price": price}}
+            self.db.books.update_one(book_to_update, updated_book)
+        except:
+            input("Invalid Input. Press enter to continue.")
+
+    def updateOrder(self, oid, uid, bid):
+        try:
+            order_to_update = {"oid": int(oid)}
+            updated_order = {"$set": {"uid": uid, "bid": bid}}
+            self.db.orders.update_one(order_to_update, updated_order)
+        except:
+            input("Invalid Input. Press enter to continue.")
+
+    def deleteUser(self, uid):
+        try:
+            user_to_delete = {"uid": int(uid)}
+            self.db.users.delete_one(user_to_delete)
+        except:
+            input("Invalid Input. Press enter to continue.")
+
+    def deleteBook(self, bid):
+        try:
+            book_to_delete = {"bid": int(bid)}
+            self.db.books.delete_one(book_to_delete)
+        except:
+            input("Invalid Input. Press enter to continue.")
+
+    def deleteOrder(self, oid):
+        try:
+            order_to_delete = {"oid": int(oid)}
+            self.db.orders.delete_one(order_to_delete)
+        except:
+            input("Invalid Input. Press enter to continue.")
+
+    def login(self, username, password):
+        user = self.db.users.find_one({"username": username, "password": password})
+        if user == None:
+            return None
+        else:
+            return (user["uid"], user["role"])
+        
+    def bookExists(self, bid):
+        try:
+            result = self.db.books.find_one({"bid": int(bid)})
+            return result
+        except:
+            pass
+
+    def getUserOrders(self, uid):
+        try:
+            user_orders = self.db.orders.find({"uid": int(uid)})
+            test = user_orders[0]
+            for order in user_orders:
+                print(order)
+        except:
+            print("No order history found.")
---
+++ main.py
@@ -0,0 +1,139 @@
+import databaseHandler as dbhm
+import logging
+
+def main():
+
+    #logging
+    logging.basicConfig(filename="Bookstore.log", level=logging.DEBUG, format='%(asctime)s :: %(message)s')
+
+    dbh = dbhm.databaseHandler()
+    logging.info("Connected to bookstore database.")
+    while True:
+        response = input("\nSelect what you would like to do.\n[L] Login\n[C] Create Account\n")
+        response = response.upper()
+        print("")
+        match response:
+            case "L":
+                username = input("Enter username: ")
+                password = input("Enter password: ")
+                result = dbh.login(username, password)
+                if result == None:
+                    input("Incorrect login. Press enter to continue.")
+                else:
+                    uid = result[0]
+                    role = result[1]
+                    break
+            case "C":
+                username = input("Enter username: ")
+                password = input("Enter password: ")
+                dbh.createUser(username, password, "user")
+                input("Account Created. Press enter and login.")
+                logging.info("User " + username + " account created.")
+            case _:
+                input("Invalid Input. Press enter to continue.")
+
+    if role == "admin":
+        print("Logged in as admin.")
+        logging.info("Admin " + username + " logged in.")
+        while True:
+            response = input("\nSelect what you would like to do.\n[AU] Add user\n[AB] Add book\n[AO] Add order\n[UU] Update user\n[UB] Update book\n[UO] Update order\n[DU] Delete user\n[DB] Delete book\n[DO] Delete order\n[SU] Show all users\n[SB] Show all books\n[SO] Show all orders\n[Q] Quit\n")
+            response = response.upper()
+            print("")
+            match response:
+                case "AU":
+                    temp_username = input("Enter username: ")
+                    password = input("Enter password: ")
+                    role = input("Enter role: ")
+                    dbh.createUser(temp_username, password, role)
+                    logging.info("User " + temp_username + " added.")
+                case "AB":
+                    title = input("Enter title: ")
+                    description = input("Enter description: ")
+                    pagecount = input("Enter pagecount: ")
+                    price = input("Enter price: ")
+                    dbh.createBook(title, description, pagecount, price)
+                    logging.info("Book " + title + " added.")
+                case "AO":
+                    #CONSIDER MAKING IT SO THAT THE ONLY WAYS ORDERS ARE ADDED IS THROUGH THE CUSTOMER'S MENU, NOT MANUALLY ENTERING IDS LIKE THIS
+                    uid = input("Enter user id: ")
+                    bid = input("Enter book id: ")
+                    dbh.createOrder(uid, bid)
+                    logging.info("Order between user ID " + uid + " and book ID " + bid + " added.")
+                case "UU":
+                    uid = input("Enter user id: ")
+                    temp_username = input("Enter new username: ")
+                    password = input("Enter new password: ")
+                    role = input("Enter new role: ")
+                    dbh.updateUser(uid, temp_username, password, role)
+                    logging.info("User " + temp_username + " updated.")
+                case "UB":
+                    bid = input("Enter book id: ")
+                    title = input("Enter new title: ")
+                    description = input("Enter new description: ")
+                    pagecount = input("Enter new pagecount: ")
+                    price = input("Enter new price: ")
+                    dbh.updateBook(bid, title, description, pagecount, price)
+                    logging.info("Book " + title + " updated.")
+                case "UO":
+                    oid = input("Enter order id: ")
+                    uid = input("Enter new user id: ")
+                    bid = input("Enter new book id: ")
+                    dbh.updateOrder(oid, uid, bid)
+                    logging.info("Order between user ID " + uid + " and book ID " + bid + " updated.")
+                case "DU":
+                    uid = input("Enter user id: ")
+                    dbh.deleteUser(uid)
+                    logging.info("User ID" + uid + " deleted.")
+                case "DB":
+                    bid = input("Enter book id: ")
+                    dbh.deleteBook(bid)
+                    logging.info("Book ID" + bid + " deleted.")
+                case "DO":
+                    oid = input("Enter order id: ")
+                    dbh.deleteOrder(oid)
+                    logging.info("Order ID" + oid + " deleted.")
+                case "SU":
+                    dbh.allUsers()
+                    input("\nPress enter to continue.")
+                case "SB":
+                    dbh.allBooks()
+                    input("\nPress enter to continue.")
+                case "SO":
+                    dbh.allOrders()
+                    input("\nPress enter to continue.")
+                case "Q":
+                    logging.info("Admin " + username + " logged out.")
+                    break
+                case _:
+                    input("Invalid Input. Press enter to continue.")
+    else:
+        print("Logged in as user.")
+        logging.info("User " + username + " logged in.")
+        while True:
+            response = input("\nSelect what you would like to do.\n[P] Purchase book\n[O] Order history\n[Q] Quit\n")
+            response = response.upper()
+            print("")
+            match response:
+                case "P":
+                    dbh.allBooks()
+                    print("")
+                    bid = input("Type the bid of the book you would like.\n")
+                    result = dbh.bookExists(bid)
+                    if result == None:
+                        input("Invalid Input. Press enter to continue.")
+                    else:
+                        dbh.createOrder(uid, bid)
+                        input("Order Placed. Press enter to continue.")
+                case "O":
+                    dbh.getUserOrders(uid)
+                    input("\nPress enter to continue.")
+                case "Q":
+                    logging.info("User " + username + " logged out.")
+                    break
+                case _:
+                    input("Invalid Input. Press enter to continue.")
+        
+
+
+if __name__ == '__main__':
+        main()
